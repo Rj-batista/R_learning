7444afc761af103371e71428554d84fc93e65fe0
WxMAMADIANxW <49515789+WxMAMADIANxW@users.noreply.github.com>
2022-06-26 22:56:15 +0200

    test

--- MC/drl_sample_project_python/drl_lib/to_do/GridWorld.py
+++ MC/drl_sample_project_python/drl_lib/to_do/GridWorld.py
@@ -1,6 +1,126 @@
-from MC.drl_sample_project_python.drl_lib.do_not_touch.contracts import SingleAgentEnv
+from MC.drl_sample_project_python.drl_lib.do_not_touch.contracts import MDPEnv, SingleAgentEnv
 import numpy as np
 import random
+
+
+class LineWorld(MDPEnv):
+    def __init__(self):
+        self.current_cell = random.randint(0, 6)
+        self.nb_cells = 7
+        self.terminal_values = [0, 6]
+        
+        
+    
+    
+    def states(self) -> np.ndarray:
+        return np.arange(self.nb_cells)
+
+    def actions(self) -> np.ndarray:
+        return np.array(0,1)
+
+    def rewards(self) -> np.ndarray:
+        return np.array(-1.0, 0.0,1.0)
+
+    def is_state_terminal(self, s: int) -> bool:
+        if s in self.terminal_values:
+            return True
+        return False
+
+    def transition_probability(self, s: int, a: int, s_p: int, r: float) -> float:
+        S,A,R = self.states() , self.actions(), self.rewards()
+        p = np.zeros((len(S),len(A), len(S), len(R)))
+        
+        for s in S:
+            if s == 1:
+                p[s, 0, s - 1, 0] = 1.0
+            else:
+                p[s, 0, s - 1, 1] = 1.0
+            
+            if s == S[-2]:
+                p[s, 1, s + 1, 2] = 1.0
+            else:
+                p[s, 1, s + 1, 1] = 1.0
+        return p[s,a,s,r]
+
+    def view_state(self, s: int):
+        for i in range(len(self.states())):
+            if i == s:
+                print('X', end=' ')
+            else:
+                print('-', end=' ')
+
+
+class GridWorld(MDPEnv):
+    def __init__(self):
+        random.seed(42)
+        self.current_cell = random.randint(0, 24)
+        self.cols, self.rows = 5, 5
+        self.nb_cells = 25
+        self.terminal_values = [4, 24]
+        
+    
+    def states(self) -> np.ndarray:
+        return np.arange(self.nb_cells)
+
+    def actions(self) -> np.ndarray:
+        return np.array(0,1,2,3)
+
+    def rewards(self) -> np.ndarray:
+        return np.array(-3.0, 0.0 , 1.0)
+
+    def is_state_terminal(self, s: int) -> bool:
+        if s in self.terminal_values:
+            return True
+        return False
+
+    def transition_probability(self, s: int, a: int, s_p: int, r: float) -> float:
+        S,A,R = self.states() , self.actions(), self.rewards()
+        p = np.zeros((len(S),len(A), len(S), len(R)))
+        haut = [0, 1, 2, 3, 4]
+        bas = [20, 21, 22, 23, 24]
+        gauche = [0, 5, 10, 15, 20]
+        droite = [4, 9, 14, 19, 24]
+        
+        for s in self.states():
+            #gauche
+            if s in gauche:
+                p[s, 1, s-1, 1]= 0.0
+            else:
+                p[s, 1, s-1, 1]= 1.0
+            #haut
+            if s in haut:
+                p[s, 2, s-self.rows, 1]= 0.0
+            elif s == 9:
+                p[s, 2, s-self.rows, 0]= 1.0
+            else:
+                p[s, 2, s-self.rows, 1]= 1.0
+            #droite
+            if s in droite:
+                p[s, 3, s-1, 1]= 0.0
+            elif s == 4:
+                    p[s,3, s+1, 0] = 1.0
+            elif s == 23:
+                    p[s,3, s+1, 2] = 1.0
+            else:
+                p[s,3,s+1,1]= 1.0
+            #bas
+            if s in bas:
+                p[s,0, s+self.rows, 1] = 0.0
+            if s == 19:
+                p[s,0, s+self.rows, 2] = 1.0
+            else:
+                p[s,0, s+self.rows, 1] = 1.0
+        
+        return p[s,a,s_p,r]
+
+    def view_state(self, s: int):
+        for i in range(self.nb_cells):
+            if i % 5 == 0:
+                print('\n')
+            if i == s:
+                print('X', end=' ')
+            else:
+                print('-', end=' ')
 
 
 class GridWorld(SingleAgentEnv):
--- MC/drl_sample_project_python/drl_lib/to_do/dynamic_programming.py
+++ MC/drl_sample_project_python/drl_lib/to_do/dynamic_programming.py
@@ -1,5 +1,8 @@
+from MC.drl_sample_project_python.drl_lib.do_not_touch.contracts import MDPEnv
 from MC.drl_sample_project_python.drl_lib.do_not_touch.mdp_env_wrapper import Env1
+from MC.drl_sample_project_python.drl_lib.to_do.GridWorld import *
 from MC.drl_sample_project_python.drl_lib.do_not_touch.result_structures import ValueFunction, PolicyAndValueFunction
+from 
 
 import numpy as np
 
@@ -22,13 +25,15 @@ def line_world():
             p[s, 1, s + 1, 1] = 1.0
     return S, A, R, p
 
-def policy_evaluation_on_line_world(pi: np.ndarray,S,A,R,p) -> ValueFunction:
+def policy_evaluation_on_line_world(pi: np.ndarray) -> ValueFunction:
     """
     Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
     Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
     Returns the Value function (V(s)) of this policy
     """
-
+    S = LineWorld(MDPEnv).states()
+    A = LineWorld(MDPEnv).actions()
+    R = LineWorld(MDPEnv).rewards()
     theta = 0.0000001
     V = np.random.random((len(S),))
     V[0] = 0.0
@@ -43,7 +48,7 @@ def policy_evaluation_on_line_world(pi: np.ndarray,S,A,R,p) -> ValueFunction:
                 total = 0.0
                 for s_p in S:
                     for r in range(len(R)):
-                        total += p[s, a, s_p, r] * (R[r] + 0.999 * V[s_p])
+                        total += LineWorld(MDPEnv).transition_probability(s, a, s_p, r) * (R[r] + 0.999 * V[s_p])
                 total *= pi[s, a]
                 V[s] += total
             delta = max(delta, np.abs(v - V[s]))
@@ -52,14 +57,16 @@ def policy_evaluation_on_line_world(pi: np.ndarray,S,A,R,p) -> ValueFunction:
     return V
 
 
-def policy_iteration_on_line_world(pi: np.ndarray, S, A, R, p) -> PolicyAndValueFunction:
+def policy_iteration_on_line_world(pi: np.ndarray) -> PolicyAndValueFunction:
     """
     Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
     Launches a Policy Iteration Algorithm in order to find the Optimal Policy and its Value Function
     Returns the Policy (Pi(s,a)) and its Value Function (V(s))
     """
 
-
+    S = LineWorld(MDPEnv).states()
+    A = LineWorld(MDPEnv).actions()
+    R = LineWorld(MDPEnv).rewards()
     theta = 0.0000001
     V = np.random.random((len(S),))
     V[0] = 0.0
@@ -67,7 +74,7 @@ def policy_iteration_on_line_world(pi: np.ndarray, S, A, R, p) -> PolicyAndValue
 
     pi = np.random.random((len(S), (len(A))))
     for s in S:
-        pi[s] /= np.sum(pi[s])
+        pi[s] .= np.sum(pi[s])
 
     pi[0] = 0.0
     pi[len(S) - 1] = 0.0
@@ -101,7 +108,7 @@ def policy_iteration_on_line_world(pi: np.ndarray, S, A, R, p) -> PolicyAndValue
                 total = 0
                 for s_p in S:
                     for r in range(len(R)):
-                        total += p[s, a, s_p, r] * (R[r] + 0.999 * V[s_p])
+                        total += LineWorld(MDPEnv).transition_probability(s, a, s_p, r) * (R[r] + 0.999 * V[s_p])
                 if total > best_a_score:
                     best_a = a
                     best_a_score = total
@@ -113,13 +120,15 @@ def policy_iteration_on_line_world(pi: np.ndarray, S, A, R, p) -> PolicyAndValue
             return pi, V
 
 
-def value_iteration_on_line_world(pi: np.ndarray, S, A, R, p) -> PolicyAndValueFunction:
+def value_iteration_on_line_world(pi: np.ndarray) -> PolicyAndValueFunction:
     """
     Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
     Launches a Value Iteration Algorithm in order to find the Optimal Policy and its Value Function
     Returns the Policy (Pi(s,a)) and its Value Function (V(s))
     """
-
+    S = LineWorld(MDPEnv).states()
+    A = LineWorld(MDPEnv).actions()
+    R = LineWorld(MDPEnv).rewards()
     theta = 0.0000001
     V = np.random.random((len(S),))
     V[0] = 0.0
@@ -135,7 +144,7 @@ def value_iteration_on_line_world(pi: np.ndarray, S, A, R, p) -> PolicyAndValueF
                 for s_p in S:
                     new_value = 0
                     for r in range(len(R)):
-                        total += p[s, a, s_p, r] * (R[r] + 0.999 * V[s_p])
+                        total += LineWorld(MDPEnv).transition_probability(s, a, s_p, r) * (R[r] + 0.999 * V[s_p])
                 total *= pi[s, a]
                 V[s] += total
                 if total > new_value:
@@ -154,6 +163,11 @@ def policy_evaluation_on_grid_world() -> ValueFunction:
     Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
     Returns the Value function (V(s)) of this policy
     """
+    S = LineWorld(MDPEnv).states()
+    A = LineWorld(MDPEnv).actions()
+    R = LineWorld(MDPEnv).rewards()
+    
+    
 
 
 
